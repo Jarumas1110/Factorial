fb738deb52a0c748b9626cf81d8b5e0087c0a74a
Jarumas1110 <64011065@kmitl.ac.th>
2021-08-30 23:40:13 +0700

    Add project files.

---
+++ Factorial/Source.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+    scanf_s("%d", &n);
+    int x = factorial(n);
+    printf("%d", x);
+    return 0;
+}
